255344f6dbfcdba6c8e9677ead84cb3f4e40610a
w-devin <Mr.wyw@foxmail.com>
2024-03-20 21:14:31 +0800

    fix. build on other platforms

--- go.mod
+++ go.mod
@@ -7,12 +7,10 @@ require (
 	github.com/otiai10/copy v1.14.0
 	github.com/rodrigocfd/windigo v0.0.0-20230809154420-8faa606d9f5f
 	github.com/stretchr/testify v1.8.4
-	golang.org/x/net v0.18.0
 	golang.org/x/sys v0.14.0
 )
 
 require (
-	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
---
+++ sqlite3/sqlite3_windows.go
@@ -0,0 +1,149 @@
+package sqlite3
+
+import (
+	"C"
+	"fmt"
+	"github.com/w-devin/poketto/assets"
+	"github.com/w-devin/poketto/file"
+	"log"
+	"os"
+	"sync"
+)
+
+const (
+	SQLITE3DLL = "sqlite3.dll"
+)
+
+var (
+	instanceCountLock sync.Mutex
+	instanceCount     = 0
+)
+
+func Init() {
+	if !file.IsFileExists(SQLITE3DLL) {
+		sqlite3Dll := assets.GetSqliteDll()
+		sqliteDll, err := sqlite3Dll.ReadFile(SQLITE3DLL)
+		if err != nil {
+			log.Fatalf("failed to got %s, %v", SQLITE3DLL, err)
+		}
+
+		err = os.WriteFile(SQLITE3DLL, sqliteDll, os.ModePerm)
+		if err != nil {
+			log.Fatalf("failed to put %s done, %v", SQLITE3DLL, err)
+		}
+	}
+}
+
+func OpenDatabase(baseName, dbKey string) (*SQLiteBase, error) {
+	Init()
+	instanceCountLock.Lock()
+	instanceCount += 1
+	instanceCountLock.Unlock()
+
+	db := &SQLiteBase{}
+	err := sqlite3_open(baseName, &db.database)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute sqlite3_open, %v", err)
+	}
+
+	if len(dbKey) != 0 {
+		err := sqlite3_key(&db.database, dbKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to execute sqlite3_key, %v", err)
+		}
+	}
+
+	return db, nil
+}
+
+func (db *SQLiteBase) Close() {
+	instanceCountLock.Lock()
+	instanceCount -= 1
+	instanceCountLock.Unlock()
+	if instanceCount <= 0 {
+		if file.IsFileExists(SQLITE3DLL) {
+			_ = os.Remove(SQLITE3DLL)
+		}
+	}
+}
+
+func (db *SQLiteBase) ExecuteQuery(query string) (fields []string, ret []map[string]interface{}, err error) {
+	statement, _, err := sqlite3_prepare_v2(&db.database, query)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to execute sqlite3_prepare_v2, %v", err)
+	}
+	defer sqlite3_finalize(statement)
+
+	for {
+		row, err := db.ReadNextRow(statement, &fields)
+		if err != nil {
+			return fields, ret, fmt.Errorf("failed to Read New Row, %v", err)
+		}
+
+		if row == nil {
+			break
+		}
+		ret = append(ret, row)
+	}
+
+	return fields, ret, nil
+}
+
+func (db *SQLiteBase) executeNonQuery(query string) {
+	//  todo: use sqlte3_exec
+	/*
+		IntPtr error;
+		sqlite3_exec(database, StringToPointer(query), IntPtr.Zero, IntPtr.Zero, out error);
+		if (error != IntPtr.Zero)
+			throw new Exception("Error with executing non-query: \"" + query + "\"!\n" + PointerToString(sqlite3_errmsg(error)));
+	*/
+	return
+}
+
+func (db *SQLiteBase) GetResultFields(statement uintptr) ([]string, error) {
+	columnCount, err := sqlite3_column_count(statement)
+	if err != nil {
+		return nil, err
+	}
+
+	var columnNames []string
+	for i := 0; i < columnCount; i++ {
+		columnNames = append(columnNames, sqlite3_column_name(statement, i))
+	}
+
+	return columnNames, nil
+}
+
+func (db *SQLiteBase) ReadNextRow(statement uintptr, fields *[]string) (map[string]interface{}, error) {
+	resultType := sqlite3_step(statement)
+	if resultType != SQLiteRow {
+		return nil, nil
+	}
+
+	ret := make(map[string]interface{})
+	var err error
+	if fields == nil || len(*fields) == 0 {
+		*fields, err = db.GetResultFields(statement)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for i := 0; i < len(*fields); i++ {
+		columnType := sqlite3_column_type(statement, i)
+		switch columnType {
+		case SQLiteDataTypesInt:
+			ret[(*fields)[i]] = sqlite3_column_int(statement, i)
+		case SQLiteDataTypesFloat:
+			ret[(*fields)[i]] = sqlite3_column_double(statement, i)
+		case SQLiteDataTypesText:
+			ret[(*fields)[i]] = sqlite3_column_text(statement, i)
+		case SQLiteDataTypesBlob:
+			ret[(*fields)[i]] = sqlite3_column_blob(statement, i)
+		default:
+			ret[(*fields)[i]] = ""
+		}
+	}
+
+	return ret, nil
+}
