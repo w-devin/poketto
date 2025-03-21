251d8c8eebc225cd0236aa03fabd0ff1c996d6ca
w-devin <Mr.wyw@foxmail.com>
2025-03-21 09:26:44 +0800

    feat. improve logger

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -3,7 +3,7 @@ package logger
 import "testing"
 
 func TestLogger(t *testing.T) {
-	Logger.SetFormatter(GetDefaultTextFormatter(false, "hello"))
+	Logger.SetFormatter(GetDefaultTextFormatter(false, ""))
 
 	Logger.Infof("test")
 }
